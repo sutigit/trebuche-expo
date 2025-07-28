1c6ee043a51df24ffb545b844e7e53077b199804
sutigit <panyanilben@gmail.com>
2025-07-28 07:58:49 +0300

    update

--- app/(tabs)/bots.tsx
+++ app/(tabs)/bots.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
+import { Heading } from "@/components/ui/heading"
+import { StatusBar } from "expo-status-bar";
 
 export default function BotsScreen() {
   return (
-    <View>
-      <Text>bots</Text>
-    </View>
+    <Box className="flex-1 pt-40 items-center bg-zinc-900">
+      <StatusBar style="light" />
+
+    </Box>
   )
 }
