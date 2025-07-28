d23a574792da48ef6db2cdb020ea393745a0f73c
sutigit <panyanilben@gmail.com>
2025-07-28 16:55:19 +0300

    update

--- components/BotCard.tsx
+++ components/BotCard.tsx
@@ -5,6 +5,7 @@ import { Heading } from "@/components/ui/heading"
 import { Tables } from '@/lib/supabase.types'
 import colors from 'tailwindcss/colors';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { Pressable } from './ui/pressable';
 
 export default function BotCard({ bot }: { bot: Tables<'default_bots'> }) {
     return (
@@ -19,26 +20,26 @@ export default function BotCard({ bot }: { bot: Tables<'default_bots'> }) {
                 </Text>
             </Box>
             <Box className="flex-row gap-12 justify-center items-center py-8">
-                <Box className=" h-6 w-16 flex justify-center items-center gap-2">
+                <Pressable className=" h-6 w-16 flex justify-center items-center gap-2">
                     <Box className="flex-row gap-2 items-center h-5">
                         <MaterialCommunityIcons name="thumb-up" size={16} color={"rgb(229, 229, 231)"} />
-                        <Text size="xs">5</Text>
+                        <Text size="xs">0</Text>
                     </Box>
                     <Text size="xs">Tykkää</Text>
-                </Box>
+                </Pressable>
                 <Box className=" h-6 w-16 flex justify-center items-center gap-2">
                     <Box className="flex-row gap-2 items-center h-5">
                         <MaterialCommunityIcons name="brain" size={16} color={"rgb(229, 229, 231)"} />
-                        <Text size="xs">10</Text>
+                        <Text size="xs">0</Text>
                     </Box>
                     <Text size="xs">Käytetty</Text>
                 </Box>
-                <Box className=" h-6 w-16 flex justify-center items-center gap-2">
+                <Pressable className=" h-6 w-16 flex justify-center items-center gap-2">
                     <Box className="flex-row gap-2 items-center h-5">
                         <MaterialCommunityIcons name="pencil" size={16} color={"rgb(229, 229, 231)"} />
                     </Box>
                     <Text size="xs">Muokkaa</Text>
-                </Box>
+                </Pressable>
             </Box>
         </Box>
     )
